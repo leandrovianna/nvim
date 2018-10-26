71c7684e08d457fc9e8cdbf37f1896f4b64ede9b
leandrovianna <leandro97vianna@gmail.com>
2018-10-26 01:37:45 -0300

    document config script

--- config.sh
+++ config.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+# install plug - plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall
+
+# install grip to preview markdown files
+pip install grip
