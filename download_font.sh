4a302405a5ff069ef9101ec064c0151735e7fb97
leandrovianna <leandro97vianna@gmail.com>
2021-04-08 16:12:27 -0300

    https is more reliable

--- download_font.sh
+++ download_font.sh
@@ -2,7 +2,8 @@
 
 # Download Haskplex font on .fonts directory
 
-git clone git@github.com:huytd/haskplex-font.git font_cache
+# git clone git@github.com:huytd/haskplex-font.git font_cache
+git clone https://github.com/huytd/haskplex-font.git font_cache
 
 mkdir -p ~/.fonts/
 cp -r font_cache/Haskplex-Nerd ~/.fonts/
